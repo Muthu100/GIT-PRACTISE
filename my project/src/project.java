cd5b2e927e80f35803bc01ec8c13d2ee8583c49b
muthu <muthukumarvj10@gmail.com>
2022-09-16 21:40:03 +0530

    "my java project"

---
+++ my project/src/project.java
@@ -0,0 +1,21 @@
+
+
+public class project {
+
+  public static void main(String args[]) {
+	  
+	  System.out.println("my java project");
+	
+	  
+	  String name1 = new String("Muthu");
+	  String name2 = new String("Muthu");
+	  
+	  if(name1.equals(name2)) {
+		  System.out.println("Equals");
+		  
+	  }
+	  
+			  
+  
+  }
+}
